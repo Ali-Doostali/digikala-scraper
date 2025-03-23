8d0b8a738f0b44dee65f4d51653d573ec22f43ec
Ali Doostali <alidoosti630@gmail.com>
2025-03-23 15:26:36 +0330

    Extracting prices of multiple products from the Digikala website

--- scraper.py
+++ scraper.py
@@ -11,28 +11,42 @@ driver_path = "E:/projects/dgkala-scraper/chromedriver.exe"
 service = Service(driver_path)
 driver = webdriver.Chrome(service=service)
 
-try:
-    # Open the page
-    driver.get("https://www.digikala.com/product/dkp-12924184/")
-    print("صفحه باز شد!")
-    
-    # Wait for the page to load completely
-    time.sleep(5)
+# Product List
+products = [
+    {"name": "Samsung Galaxy S23 FE", "url": "https://www.digikala.com/product/dkp-12924184/"},
+    {"name": "Samsung Galaxy A54", "url": "https://www.digikala.com/product/dkp-11346304/"},
+    {"name": "Samsung Galaxy S24 Ultra", "url": "https://www.digikala.com/product/dkp-13714776/"},
+    {"name": "Samsung Galaxy A55", "url": "https://www.digikala.com/product/dkp-14851833/"}
+]
 
-    # Find the price tag
-    price_tag = driver.find_element(By.CSS_SELECTOR, "span.text-h4")
-    if price_tag:
-        price = price_tag.text.strip()
-        print(f"price: {price}")
+# List to store data
+data = []
+
+try:
+    for product in products:
+        print(f"Scrapping: {product['name']}")
         
-        #  Save to CSV
-        data = {"product": "Samsung S23 FE phone", "price": price}
-        df = pd.DataFrame([data])
-        df.to_csv("prices.csv", index=False, encoding="utf-8-sig")
-        print("save to price.csv!")
-    else:
-        print("Price tag not found!")
+        # Open the product page
+        driver.get(product["url"])
+        time.sleep(5)  # Wait for the page to load
+
+        # Find the price tag
+        try:
+            price_tag = driver.find_element(By.CSS_SELECTOR, "span.text-h4")
+            price = price_tag.text.strip()
+            print(f"price {product['name']}: {price}")
+        except:
+            price = "Price not found"
+            print(f"price for {product['name']} Not found!")
+
+        # Add to data list
+        data.append({"product": product["name"], "price": price})
 
 finally:
-    # Close the browser
+    # Add to data list
     driver.quit()
+
+# Save data in CSV
+df = pd.DataFrame(data)
+df.to_csv("prices.csv", index=False, encoding="utf-8-sig")
+print("Data saved in prices.csv!")
